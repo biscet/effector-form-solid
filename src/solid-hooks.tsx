3fb17d4c1f8e782fc9ad229adc99a629c951e3ec
Егор Прохоров <befealmellow@gmail.com>
2023-12-25 14:35:40 +0400

    описал хуки

--- src/solid-hooks.tsx
+++ src/solid-hooks.tsx
@@ -1,4 +1,37 @@
 import { useUnit } from "effector-solid";
+import {
+  ValidationError,
+  AnyFormValues,
+  Form
+} from "./types"
+
+type ErrorTextMap = {
+  [key: string]: string
+}
+
+type AddErrorPayload = { rule: string; errorText?: string }
+
+type ConnectedField<Value> = {
+  name?: string
+  value?: Value
+  errors?: ValidationError<Value>[]
+  firstError?: ValidationError<Value> | null
+  hasError?: () => boolean
+  onChange?: (v: Value) => Value
+  onBlur?: (v: void) => void
+  errorText?: (map?: ErrorTextMap) => string
+  addError?: (p: AddErrorPayload) => AddErrorPayload
+  validate?: (v: void) => void
+  isValid?: boolean
+  isDirty?: boolean
+  isTouched?: boolean
+  touched?: boolean
+  reset?: (v: void) => void
+  set?: (v: Value) => Value
+  resetErrors?: (v: void) => void
+  onChangeField?: (e: any) => void
+}
+
 
 const isEmpty = (obj: any): Boolean =>
   [Object, Array].includes((obj || {}).constructor) &&
@@ -7,7 +40,7 @@ const isEmpty = (obj: any): Boolean =>
 /**
  * @group Hooks
  */
-export const useFormSignals = (currentForm: any, signals: any) => {
+export function useFormSignals<F extends AnyFormValues>(currentForm: Form<F>, signals:  Array<string>) {
   const labelsForSignals = Object.keys(currentForm.fields);
   let fields = {};
 
@@ -30,11 +63,11 @@ export const useFormSignals = (currentForm: any, signals: any) => {
   return fields;
 };
 
-export const useForm = (currentForm: any, signals: any) => {
-  const form: any = useUnit(currentForm);
+export function useForm<F extends AnyFormValues>(currentForm: Form<F>, signals: Array<string>) {
+  const form = useUnit(currentForm);
   const fields = useFormSignals(currentForm, signals);
 
-  const submitHandler = (e: any) => {
+  const submitHandler = (e: any): void  => {
     e.preventDefault();
     form.submit();
   };
@@ -46,13 +79,15 @@ export const useForm = (currentForm: any, signals: any) => {
   };
 };
 
-export const useFormField = (currentForm: any, fieldName: any) => {
+export function useFormField<F extends AnyFormValues>(currentForm: Form<F>, fieldName: string) {
   const field = useUnit(currentForm.fields[fieldName]);
 
-  const onChangeField = (e: any) => field.onChange(e.target.value);
+  const onChangeField = (e: any): void => {
+    field.onChange(e.target.value)
+  };
 
   return {
     ...field,
     onChangeField
-  };
+  } as ConnectedField<any> ;
 };
